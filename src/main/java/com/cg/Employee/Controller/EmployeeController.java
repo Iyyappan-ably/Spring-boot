bb6f4605de522b4950f40636ad637bed4ec269b3
Iyyappan-ably <aiyappan691@gmail.com>
2021-06-06 12:01:46 +0530

    added Employee class

--- src/main/java/com/cg/Employee/Controller/EmployeeController.java
+++ src/main/java/com/cg/Employee/Controller/EmployeeController.java
@@ -1,16 +1,24 @@
 package com.cg.Employee.Controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.Employee.Entity.Employee;
+
 @RestController
 public class EmployeeController {
 
+	private Employee emp;
+	
 	@GetMapping("/getEmployeeDetails")
-	public String getData() {
+	public ResponseEntity<Employee> getData() {
 		
-		return "this is a Employee";
+		emp = new Employee("lara",21);
+		return new ResponseEntity<>(emp,HttpStatus.OK);
+		
+		
 	}
 	
 	
